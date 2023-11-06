dee28a87378665957e6539ecee71c927c67935e6
davibernardes035 <daviber20@gmail.com>
2023-11-06 17:50:31 -0300

    Criação do modal

--- src/screens/HomePage/index.js
+++ src/screens/HomePage/index.js
@@ -1,4 +1,10 @@
-import * as React from 'react';
+import React, {useState} from 'react';
+import {
+  Modal,
+  TouchableOpacityComponent,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {
   Button,
   Container,
@@ -9,30 +15,91 @@ import {
   SubTitle,
   Title,
   TextButton,
+  ContainerModal,
+  TextModal,
+  InputModal,
+  TextModalButton,
 } from './styles';
 import More from '../../assets/More.svg';
 
-const HomePage = ({navigation}) => {
+const HomePage = () => {
+  const [modal, setModal] = useState(false);
+  const [modalButton, setModalButton] = useState('#000000');
+
+  colorButton = [
+    '#5CD859',
+    '#24A6D9',
+    '#5958D9',
+    '#8022D9',
+    '#D159D8',
+    '#D85963',
+    '#D88559',
+  ];
+
+  const renderButtonColor = () => {
+    return colorButton.map(color => {
+      return (
+        <TouchableOpacity
+          key={color}
+          style={{
+            backgroundColor: color,
+            width: 30,
+            height: 30,
+            margin: 10,
+            borderRadius: 8,
+          }}
+          onPress={() => setModalButton(color)}
+        />
+      );
+    });
+  };
   return (
-    <Container>
-      <ContainerTitle>
-        <Line />
-        <ContainerText>
-          <Title>Davi</Title>
-          <SubTitle>List</SubTitle>
-        </ContainerText>
-        <Line />
-      </ContainerTitle>
-      <ContainerButton>
-        <Button
-          onPress={() => {
-            navigation.navigate('List');
-          }}>
-          <More />
-        </Button>
-        <TextButton>Adicionar à lista</TextButton>
-      </ContainerButton>
-    </Container>
+    <>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modal}
+        onRequestClose={() => {
+          setModal(!modal);
+        }}>
+        <ContainerModal>
+          <TextModal>Criar Lista</TextModal>
+          <InputModal placeholder="Nome da lista"></InputModal>
+          <View style={{flexDirection: 'row'}}>{renderButtonColor()}</View>
+          <TouchableOpacity
+            style={{
+              width: '80%',
+              height: 47,
+              backgroundColor: modalButton,
+              borderRadius: 8,
+              justifyContent: 'center',
+            }}
+            onPress={() => setModal(false)}
+            >
+            <TextModalButton>Criar</TextModalButton>
+          </TouchableOpacity>
+        </ContainerModal>
+      </Modal>
+      <Container>
+        <ContainerTitle>
+          <Line />
+          <ContainerText>
+            <Title>Davi</Title>
+            <SubTitle>List</SubTitle>
+          </ContainerText>
+          <Line />
+        </ContainerTitle>
+        <ContainerButton>
+          <Button
+            onPress={() => {
+              setModal(true);
+            }}>
+            <More />
+          </Button>
+          <TextButton>Adicionar à lista</TextButton>
+        </ContainerButton>
+      </Container>
+    </>
   );
 };
 
--- src/screens/HomePage/styles.js
+++ src/screens/HomePage/styles.js
@@ -27,6 +27,13 @@ export const ContainerText = styled.View`
   flex-direction: row;
 `;
 
+export const ContainerModal = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
 export const SubTitle = styled.Text`
   color: #72bfe8;
   font-weight: 400;
@@ -52,4 +59,27 @@ export const Button = styled.TouchableOpacity`
 export const TextButton = styled.Text`
   font-size: 10px;
   color: #72bfe8;
+`;
+
+export const TextModal = styled.Text`
+  font-size: 28px;
+  color: #000000;
+  font-weight: 700;
+  margin-bottom: 14px;
+`;
+
+export const InputModal = styled.TextInput`
+  width: 80%;
+  color: #000;
+  height: 47px;
+  border: 1px solid #000;
+  border-radius: 8px;
+  font-size: 18px;
+`;
+
+export const TextModalButton = styled.Text`
+  font-size: 20px;
+  color: #FFFFFF;
+  font-weight: bold;
+  text-align: center;
 `;
